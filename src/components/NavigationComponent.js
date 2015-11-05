7e89cc8bf92c65c7faa6bdffb55b4ee1ecbcf973
Yamagishi Kazutoshi <ykzts@desire.sh>
2015-11-06 08:53:05 +0900

    navbar の構造を修正

--- src/components/NavigationComponent.js
+++ src/components/NavigationComponent.js
@@ -34,27 +34,29 @@ class NavigationComponent extends React.Component {
   render() {
     return (
       <nav className='navbar navbar-default'>
-        <header className='navbar-header'>
-          <button type='button' className={`navbar-toggle${this.state.expanded ? ' collapsed' : ''}`} data-toggle='collapse' data-target='#global-navbar' aria-expanded={this.state.expanded} onClick={this.handleClick.bind(this)}>
-            <span className='sr-only'>Toggle navigation</span>
-            <span className='icon-bar'></span>
-            <span className='icon-bar'></span>
-            <span className='icon-bar'></span>
-          </button>
-          <NavLink routeName='root' className='navbar-brand'>Gy</NavLink>
-        </header>
-        <div className={`navbar-collapse collapse${this.state.expanded ? ' in' : ''}`} id='global-navbar'>
-          <ul className='nav navbar-nav'>
-            <li>
-              <NavLink routeName='about' activeClass='active'>About</NavLink>
-            </li>
-            <li>
-              <NavLink routeName='images' activeClass='active'>Images</NavLink>
-            </li>
-            <li>
-              <NavLink routeName='settings' activeClass='active'>Settings</NavLink>
-            </li>
-          </ul>
+        <div className='container'>
+          <header className='navbar-header'>
+            <button type='button' className={`navbar-toggle${this.state.expanded ? ' collapsed' : ''}`} data-toggle='collapse' data-target='#global-navbar' aria-expanded={this.state.expanded} onClick={this.handleClick.bind(this)}>
+              <span className='sr-only'>Toggle navigation</span>
+              <span className='icon-bar'></span>
+              <span className='icon-bar'></span>
+              <span className='icon-bar'></span>
+            </button>
+            <NavLink routeName='root' className='navbar-brand'>Gy</NavLink>
+          </header>
+          <div className={`navbar-collapse collapse${this.state.expanded ? ' in' : ''}`} id='global-navbar' aria-expanded={this.state.expanded}>
+            <ul className='nav navbar-nav'>
+              <li>
+                <NavLink routeName='about' activeClass='active'>About</NavLink>
+              </li>
+              <li>
+                <NavLink routeName='images' activeClass='active'>Images</NavLink>
+              </li>
+              <li>
+                <NavLink routeName='settings' activeClass='active'>Settings</NavLink>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     );
