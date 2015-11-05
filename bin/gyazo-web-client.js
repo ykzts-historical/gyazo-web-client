815894944d2a843f391a52d8a579682c4dd29999
Yamagishi Kazutoshi <ykzts@desire.sh>
2015-11-05 20:15:42 +0900

    koa を使うように

--- bin/gyazo-web-client.js
+++ bin/gyazo-web-client.js
@@ -1,8 +1,7 @@
 #!/bin/node/env node
 
 import { env } from 'process';
-import Server from '../src/server';
+import server from '../src/server';
 
-let server = new Server();
 let port = env.PORT || 8080;
 server.listen(port);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
-import fs from 'fs';
-import http from 'http';
 import serialize from 'serialize-javascript';
+import Koa from 'koa';
+import convert from 'koa-convert';
+import serve from 'koa-static';
 import React from 'react';
 import ReactDOM from 'react-dom/server';
 import { navigateAction } from 'fluxible-router';
@@ -8,69 +9,21 @@ import { createElementWithContext } from 'fluxible-addons-react';
 import application from './application';
 import RootComponent from './components/RootComponent';
 
-class Server extends http.Server {
-  constructor(requestListener) {
-    super(requestListener);
-    this.staticFileDirectory = `${__dirname}/../public`;
-    this.on('request', this.handleRequest);
-  }
+let server = new Koa();
 
-  handleRequest(request, response) {
-    return this.serveStaticFile(request, response, () => {
-      return this.executeApplication(request, response, () => {
-        return this.handleError(request, response);
-      });
-    });
-  }
+server.use(convert(serve(`${__dirname}/../public`)));
 
-  handleError(request, response) {
-    response.writeHeader(404, {
-      'Content-type': 'text/plain'
-    });
-    response.write('not found.\n');
-    response.end();
-  }
+server.use(async (ctx, next) => {
+  let context = application.createContext();
+  await context.executeAction(navigateAction, {
+    url: ctx.url
+  });
+  let html = ReactDOM.renderToStaticMarkup(React.createElement(RootComponent, {
+    state: `window.App=${serialize(application.dehydrate(context))};`,
+    markup: ReactDOM.renderToString(createElementWithContext(context)),
+    context: context.getComponentContext()
+  }));
+  ctx.body = `<!DOCTYPE html>\n${html}`;
+});
 
-  serveStaticFile(request, response, callback) {
-    let absolutePath = `${this.staticFileDirectory}${request.url}`;
-    fs.readFile(absolutePath, function(error, data) {
-      if (error instanceof Error) {
-        return callback(request, response);
-      }
-      response.writeHeader(200, {
-        'Content-type': {
-          'favcion.ico': 'image/x-icon',
-          '/bundle.js': 'application/javascript'
-        }[request.url]
-      });
-      response.write(data);
-      return response.end();
-    });
-  }
-
-  executeApplication(request, response, callback) {
-    let context = application.createContext();
-
-    context.executeAction(navigateAction, {
-      url: request.url
-    }, function(error) {
-      if (error instanceof Error) {
-        callback(request, response);
-        return;
-      }
-      let html = ReactDOM.renderToStaticMarkup(React.createElement(RootComponent, {
-        state: `window.App=${serialize(application.dehydrate(context))};`,
-        markup: ReactDOM.renderToString(createElementWithContext(context)),
-        context: context.getComponentContext()
-      }));
-      response.writeHeader(200, {
-        'Content-type': 'text/html'
-      });
-      response.write('<!DOCTYPE html>\n');
-      response.write(html);
-      response.end();
-    });
-  }
-}
-
-export default Server;
+export default server;
