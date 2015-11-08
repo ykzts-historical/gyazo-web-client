13b15ead0a24f28d1531576f9c9e71d16c3906cf
Yamagishi Kazutoshi <ykzts@desire.sh>
2015-11-08 16:56:07 +0900

    lint をかけてから build をはじめるように

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,14 +1,15 @@
 import { env } from 'process';
 import gulp from 'gulp';
+import eslint from 'gulp-eslint';
 import browserify from 'browserify';
 import babelify from 'babelify';
 import source from 'vinyl-source-stream';
 
 const DEBUG = env.NODE_ENV !== 'production';
 
-gulp.task('build', ['build:script']);
+gulp.task('build', ['build:client']);
 
-gulp.task('build:script', function() {
+gulp.task('build:client', ['lint'], function() {
   let src = browserify({
     entries: ["./src/client.js"],
     debug: DEBUG
@@ -16,6 +17,13 @@ gulp.task('build:script', function() {
   return src
     .pipe(source('bundle.js'))
     .pipe(gulp.dest(`${__dirname}/public`));
+});
+
+gulp.task('lint', function() {
+  return gulp.src(['./config/routes.js', './src/**/*.js'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
 
 gulp.task('watch', ['build'], function() {
