d3d322f600580973e9285d523683c34bf8649575
Yamagishi Kazutoshi <ykzts@desire.sh>
2015-11-29 06:38:25 +0900

    `async function` で無名関数を使わないように

--- src/actions/saveUploadedImageAction.js
+++ src/actions/saveUploadedImageAction.js
@@ -1,12 +1,12 @@
 import Image from '../models/Image';
 
-let saveUploadedImageAction = async (context, payload) => {
+async function saveUploadedImageAction(context, payload) {
   let { fileName, uri } = payload;
   let uploadedAt = (payload.uploadedAt instanceof Date ? payload.uploadedAt : new Date()).toJSON();
   let image = new Image();
   await image.ready;
   let images = [await image.save({ fileName, uri, uploadedAt })];
   context.dispatch('PREPEND_UPLOADED_IMAGES', { images });
-};
+}
 
 export default saveUploadedImageAction;
