165062bb74aed48e3c7231cc9e0de36d59fe2f49
Yamagishi Kazutoshi <ykzts@desire.sh>
2015-12-10 00:03:26 +0900

    直近にアップロードした画像のみを表示させるように
    アップロード画面にアップロードした画像を一覧表示させていたが画像が多くなりすぎると早晩破綻することは明白なので、直近の二十四件のみを表示させるよ
    うにした。
    
    ページ送り、もしくは autopager ができるようにするべきだが、とりあえずはかんがえないことに。

--- src/actions/loadUploadedImagesAction.js
+++ src/actions/loadUploadedImagesAction.js
@@ -4,6 +4,10 @@ async function loadUploadedImagesAction(context, payload) {
   let image = new Image();
   await image.ready;
   let images = await image.all();
+  images.sort((previousImage, image) =>
+    previousImage.uploadedAt < image.uploadedAt ? 1 : -1
+  );
+  images = images.slice(0, 24);
   context.dispatch('SET_UPLOADED_IMAGES', { images });
 }
 
